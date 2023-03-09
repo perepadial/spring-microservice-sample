af83492c3295feb15e243ffa4a4415dff9f27762
Pere <94077765+perepadial@users.noreply.github.com>
2023-03-09 09:53:46 +0100

    get & put customers

--- src/main/java/com/travis/Main.java
+++ src/main/java/com/travis/Main.java
@@ -2,17 +2,37 @@ package com.travis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @SpringBootApplication
 @RestController
+@RequestMapping("api/v1/customers")
 public class Main {
+
+    private final CustomerRepository customerRepository;
+
+    public Main(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
-    @GetMapping("/greet")
-    public String greet(){
-        return "Hello World !";
+    @GetMapping()
+    public List<Customer> getCustomers(){
+        return this.customerRepository.findAll();
+    }
+
+    record CustomerRequest(String name, String email, Integer age){};
+
+    @PostMapping
+    public void addCustomer(@RequestBody CustomerRequest customerRequest){
+        Customer customer = new Customer();
+        customer.setName(customerRequest.name());
+        customer.setEmail(customerRequest.email());
+        customer.setAge(customerRequest.age());
+        customerRepository.save(customer);
     }
 }
